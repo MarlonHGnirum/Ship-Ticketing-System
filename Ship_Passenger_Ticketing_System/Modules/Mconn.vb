8ecdd04894583adc17610d7074193b4fad5bbf3d
Marlon H. Muring <44626944+MarlonHGnirum@users.noreply.github.com>
2022-05-01 14:56:27 +0800

    Add files via upload

---
+++ Ship_Passenger_Ticketing_System/Modules/Mconn.vb
@@ -0,0 +1,6 @@
+﻿Imports MySql.Data.MySqlClient
+Module Module1
+    Public con As New MySqlConnection("Server=localhost;username=root;password=;database=dbship;")
+    Public con1 As New MySqlConnection("Server=localhost;username=root;password=;database=dbship;")
+End Module
+
